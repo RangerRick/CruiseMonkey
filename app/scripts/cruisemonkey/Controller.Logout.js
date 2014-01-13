968a449b8e66486c442d75bdda175728866488f1
Benjamin Reed <rangerrick@befunk.com>
2014-01-13 11:21:21 -0500

    remove landscape support, add custom URL, clean up logout controller

--- app/scripts/cruisemonkey/Controller.Logout.js
+++ app/scripts/cruisemonkey/Controller.Logout.js
@@ -2,12 +2,11 @@
 	'use strict';
 
 	angular.module('cruisemonkey.controllers.Logout', ['cruisemonkey.Logging', 'cruisemonkey.User'])
-	.controller('CMLogoutCtrl', ['$scope', '$rootScope', '$routeParams', '$location', 'UserService', 'LoggingService', function($scope, $rootScope, $routeParams, $location, UserService, log) {
+	.controller('CMLogoutCtrl', ['$rootScope', '$location', 'UserService', 'LoggingService', function($rootScope, $location, UserService, log) {
 		log.info('Initializing CMLogoutCtrl');
 		$rootScope.title = "Logging Out";
 		$rootScope.user = UserService.reset();
 
-		$location.replace();
-		$location.path('/events/official');
+		$location.path('/events/official').replace();
 	}]);
 }());
