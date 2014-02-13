9c978ea32ce04704aff764835255b5575229f35b
Benjamin Reed <rangerrick@befunk.com>
2014-02-13 11:18:13 -0500

    *working* script for updating ionic ;)

--- update-ionic.sh
+++ update-ionic.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ev
 
-cp "$0" /tmp/ >/dev/null 2>&1
+cp "$0" /tmp/ || :
 
-pushd `dirname $0` >/dev/null 2>&1
+pushd `dirname $0`
 MYPATH=`pwd`
 MYNAME=`basename $0`
 
@@ -15,18 +15,23 @@ COMMIT="unknown"
 CMDIR="$1"
 echo "CruiseMonkey is '$CMDIR'."
 
-pushd "${CMDIR}/ionic" >/dev/null 2>&1
+pushd "${CMDIR}/ionic"
 	rm -rf dist
 	git pull
+	npm install
 	grunt
 	COMMIT=`git log -1 | grep -E '^commit' | cut -d' ' -f2`
-popd >/dev/null 2>&1
+popd
 
-git checkout ionic-pristine
-rsync -avr ionic/dist/js/ionic* app/scripts/angular-3rdparty/
-rsync -avr ionic/dist/fonts/ app/fonts/
-rsync -avr ionic/scss/ app/styles/
-git add app/scripts/angular-3rdparty/ionic* app/fonts app/styles/_* app/styles/ionic* app/styles/themes
-git commit -m "pristine ionic $COMMIT"
-git checkout master
-git merge ionic-pristine
+pushd "${CMDIR}"
+	git stash
+	git checkout ionic-pristine
+	rsync -avr ionic/dist/js/ionic* app/scripts/angular-3rdparty/
+	rsync -avr ionic/dist/fonts/ app/fonts/
+	rsync -avr ionic/scss/ app/styles/
+	git add app/scripts/angular-3rdparty/ionic* app/fonts app/styles/_* app/styles/ionic* app/styles/themes
+	git commit -m "pristine ionic $COMMIT"
+	git checkout master
+	git merge ionic-pristine
+	git stash pop
+popd
