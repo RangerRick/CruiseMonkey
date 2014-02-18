ed43cf29c0f5cfb2bb38e7f0b1a6ecc9b7ad91b2
Benjamin Reed <rangerrick@befunk.com>
2014-02-17 23:32:00 -0500

    attempt to solve ssl issues

--- platforms/android/src/com/raccoonfink/cruisemonkey/CruiseMonkey.java
+++ platforms/android/src/com/raccoonfink/cruisemonkey/CruiseMonkey.java
@@ -21,19 +21,38 @@ package com.raccoonfink.cruisemonkey;
 
 import org.apache.cordova.Config;
 import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebViewClient;
 
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
 
 public class CruiseMonkey extends CordovaActivity 
 {
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        super.init();
-        // Set by <content src="index.html" /> in config.xml
-        super.loadUrl(Config.getStartUrl());
-        //super.loadUrl("file:///android_asset/www/index.html")
-    }
+	public class MyWebViewClient extends CordovaWebViewClient {
+
+		public MyWebViewClient(final CordovaInterface cordova) {
+			super(cordova);
+		}
+
+		@Override
+		public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+			handler.proceed();
+		}
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		super.init();
+		this.appView.setWebViewClient(new MyWebViewClient(this));
+
+		// Set by <content src="index.html" /> in config.xml
+		super.loadUrl(Config.getStartUrl());
+		//super.loadUrl("file:///android_asset/www/index.html")
+	}
 }
 
