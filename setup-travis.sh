e32caa0cbde43c9000c0b3dd8b0d7d4de6361699
Benjamin Reed <ranger@opennms.org>
2014-12-03 21:59:54 -0500

    new test directory

--- setup-travis.sh
+++ setup-travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 node_modules/.bin/pouchdb-server -m -p 5999 &
-perl -pi -e 's,5984,5999,g' test/mock/*
+perl -pi -e 's,5984,5999,g' spec/*
 #sleep 5
 #pushd couchdb
 #	couchapp --verbose push http://localhost:5999/test-pristine
