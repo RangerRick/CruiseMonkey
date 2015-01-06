24e391ff386f5510b52fa58595d8d3bd2cfc90f9
Benjamin Reed <ranger@opennms.org>
2015-01-06 17:51:31 -0500

    a few small updates to build script stuff

--- bin/sign-android.sh
+++ bin/sign-android.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+EXTRA_ARGS=""
+if [ -n "$SIGNING_PASS" ]; then
+	EXTRA_ARGS="-storepass $SIGNING_PASS -keypass $SIGNING_PASS"
+fi
+
 rsync -avr "platforms/android/ant-build/MainActivity-release-unsigned.apk" "platforms/android/ant-build/CruiseMonkey-release-signed.apk"
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/share/android/android-release-key.keystore "platforms/android/ant-build/CruiseMonkey-release-signed.apk" ranger
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/share/android/android-release-key.keystore $EXTRA_ARGS "platforms/android/ant-build/CruiseMonkey-release-signed.apk" ranger
 rm -f "platforms/android/ant-build/CruiseMonkey-release.apk"
 zipalign -v 4 "platforms/android/ant-build/CruiseMonkey-release-signed.apk" "platforms/android/ant-build/CruiseMonkey-release.apk"
