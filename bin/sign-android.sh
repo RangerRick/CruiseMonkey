1157b58c8f1d5f82006025636970b54539f4e562
Benjamin Reed <ranger@opennms.org>
2015-01-06 20:16:43 -0500

    make it more obvious which is the right apk

--- bin/sign-android.sh
+++ bin/sign-android.sh
@@ -5,7 +5,7 @@ if [ -n "$SIGNING_PASS" ]; then
 	EXTRA_ARGS="-storepass $SIGNING_PASS -keypass $SIGNING_PASS"
 fi
 
-rsync -avr "platforms/android/ant-build/MainActivity-release-unsigned.apk" "platforms/android/ant-build/CruiseMonkey-release-signed.apk"
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/share/android/android-release-key.keystore $EXTRA_ARGS "platforms/android/ant-build/CruiseMonkey-release-signed.apk" ranger
+rsync -avr "platforms/android/ant-build/MainActivity-release-unsigned.apk" "platforms/android/ant-build/CruiseMonkey-release-unaligned.apk"
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/share/android/android-release-key.keystore $EXTRA_ARGS "platforms/android/ant-build/CruiseMonkey-release-unaligned.apk" ranger
 rm -f "platforms/android/ant-build/CruiseMonkey-release.apk"
-zipalign -v 4 "platforms/android/ant-build/CruiseMonkey-release-signed.apk" "platforms/android/ant-build/CruiseMonkey-release.apk"
+zipalign -v 4 "platforms/android/ant-build/CruiseMonkey-release-unaligned.apk" "platforms/android/ant-build/CruiseMonkey-release.apk"
