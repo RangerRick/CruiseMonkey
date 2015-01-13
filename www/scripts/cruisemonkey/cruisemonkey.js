4176fd984ca573aa7e6e21cc59cec6c58a47eb30
Benjamin Reed <ranger@opennms.org>
2015-01-12 23:17:14 -0500

    feat(twitarr): add date stamp to the tweet list

--- www/scripts/cruisemonkey/cruisemonkey.js
+++ www/scripts/cruisemonkey/cruisemonkey.js
@@ -223,6 +223,24 @@
 			console.log('ERROR: ' + fromState + ' -> ' + toState, event, fromState, fromParams, toState, toParams, error);
 		});
 
+		moment.locale('en', {
+			relativeTime: {
+				future : 'in %s',
+				past : '%s ago',
+				s : 'a few seconds',
+				m : '1 minute',
+				mm : '%d minutes',
+				h : '1 hour',
+				hh : '%d hours',
+				d : '1 day',
+				dd : '%d days',
+				M : '1 month',
+				MM : '%d months',
+				y : '1 year',
+				yy : '%d years'
+			}
+		});
+
 		$rootScope.isCordova = function() {
 			if ($window.cordova) {
 				return true;
