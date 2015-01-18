f5539248067879480bf902865334e765cafde4d1
Benjamin Reed <ranger@opennms.org>
2015-01-18 16:59:37 -0500

    chore(android): update build script

--- bin/build-android.sh
+++ bin/build-android.sh
@@ -4,4 +4,5 @@ BUILD_NUMBER=`node bin/update-build-number.js`
 
 export ANDROID_VERSION_CODE="$BUILD_NUMBER"
 
-ionic build --release android && bin/sign-android.sh
+ionic build --release android && bin/sign-android.sh && \
+rsync -avr --progress platforms/android/build/outputs/apk/android-signed.apk ranger@befunk.com:~/public_html/temp/
