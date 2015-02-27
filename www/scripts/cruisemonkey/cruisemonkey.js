RangerRick/CruiseMonkey

commit 05f6e6013eeac4170ce79f88010fcf8f3610ac94

Moved the functionality of the auto_linking into twitarr, so that the html parsing is not necessary.  If we don't want to do it here, we can change the app to be app=plain, and then it won't do any markup, then we can include the twitter's javascript text api in CruiseMonkey and do it there.

www/scripts/cruisemonkey/cruisemonkey.js
@@ -488,7 +488,8 @@
 
 		$rootScope.closeKeyboard = function() {
 			inCordova.then(function() {
-				$cordovaKeyboard.close();
+				if ($cordovaKeyboard !== undefined && device.platform !== 'browser')
+				  $cordovaKeyboard.close();
 			});
 		};
 

www/scripts/cruisemonkey/Controller.Twitarr.Stream.js
@@ -57,26 +57,6 @@
 	var urlSubMatch  = /href="([^"]*)"/i;
 
 	var translateText = function(text) {
-		var result = text.match(hrefMatch), found;
-		if (result) {
-			for (var i=0; i < result.length; i++) {
-				found = result[i].match(userSubMatch);
-				if (found) {
-					//console.log('user link:', found);
-					text = text.replace(found.input, 'ng-click="openUser(\'' + found[1] + '\', \$event);"');
-				} else {
-					found = result[i].match(tagSubMatch);
-					if (found) {
-						//console.log('tag link:', found);
-						text = text.replace(found.input, 'ng-click="openTag(\'' + found[1] + '\');"');
-					} else {
-						found = result[i].match(urlSubMatch);
-						//console.log('external link:', found);
-						text = text.replace(found.input, 'ng-click="openUrl(\'' + found[1] + '\', \'_blank\');"');
-					}
-				}
-			}
-		}
 		//console.log('text=',text);
 		return text;
 	};

www/scripts/cruisemonkey/Images.js
@@ -64,7 +64,7 @@
 					});
 				}
 			}, function() {
-				deferred.reject('Not in Cordova!');
+				deferred.reject('Images: Not in Cordova!');
 			});
 
 			return deferred.promise;
@@ -93,7 +93,7 @@
 					});
 				}, onError);
 			}, function() {
-				deferred.reject('Not in Cordova!');
+				deferred.reject('Images: Not in Cordova!');
 			});
 
 			return deferred.promise;

www/scripts/cruisemonkey/Twitarr.js
@@ -326,9 +326,9 @@
 		};
 
 		var getStream = function(nextPage) {
-			var url = SettingsService.getTwitarrRoot() + 'api/v2/stream';
+			var url = SettingsService.getTwitarrRoot() + 'api/v2/stream?app=CM';
 			if (nextPage) {
-				url += '?start=' + parseInt(nextPage) + '&older_posts=true';
+				url += '&start=' + parseInt(nextPage) + '&older_posts=true';
 			}
 
 			var deferred = $q.defer();

commit 0d1edae4b4dcb78c50bfd357ea6c1644f0005062

Fixing tests I broke with change 05f6e6013eeac4170ce79f88010fcf8f3610ac94

www/scripts/cruisemonkey/cruisemonkey.js
@@ -7,6 +7,7 @@
 	/* global Camera: true */
 	/* global StatusBar: true */
 	/* global moment: true */
+	/* global device: true */
 
 	angular.module('cruisemonkey',
 	[
@@ -488,8 +489,9 @@
 
 		$rootScope.closeKeyboard = function() {
 			inCordova.then(function() {
-				if ($cordovaKeyboard !== undefined && device.platform !== 'browser')
+				if ($cordovaKeyboard !== undefined && device.platform !== 'browser') {
 				  $cordovaKeyboard.close();
+				}
 			});
 		};
 

spec/test.cruisemonkey.Twitarr.js
@@ -184,7 +184,7 @@ describe('Twit-Arr', function() {
 	};
 
 	it('should return 5 posts', function() {
-		$httpBackend.expectGET('https://jccc5.rylath.net/api/v2/stream').respond(200, mockPageOne);
+		$httpBackend.expectGET('https://jccc5.rylath.net/api/v2/stream?app=CM').respond(200, mockPageOne);
 		twit.getStream().then(function(res) {
 			expect(res).toBeDefined();
 			expect(res.next_page).toBeDefined();
@@ -195,8 +195,8 @@ describe('Twit-Arr', function() {
 	});
 
 	xit('should return 3 posts', function() {
-		$httpBackend.expectGET('https://jccc5.rylath.net/api/v2/stream').respond(200, mockPageOne);
-		$httpBackend.expectGET('https://jccc5.rylath.net/api/v2/stream?start=1420848811355.0').respond(200, mockPageTwo);
+		$httpBackend.expectGET('https://jccc5.rylath.net/api/v2/stream?app=CM').respond(200, mockPageOne);
+		$httpBackend.expectGET('https://jccc5.rylath.net/api/v2/stream?app=CM&start=1420848811355.0').respond(200, mockPageTwo);
 		twit.getStream().then(function(res) {
 			twit.moreStream().then(function(res) {
 				expect(res).toBeDefined();