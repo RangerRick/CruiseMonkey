c6d94012d3eb1b1e76d66c51e7815fc840764fa8
Benjamin Reed <ranger@opennms.org>
2016-01-19 17:47:03 -0500

    fix image upload from android, plus lots of eslink fixes

--- bin/sign-android.sh
+++ bin/sign-android.sh
@@ -9,16 +9,17 @@ sign_jar() {
 	local JAR="$1";
 }
 
-APKFILE=`find platforms/android -name \*-debug.apk -o -name \*-release-unsigned.apk | xargs ls -1 -tr | tail -n 1`
-SIGNME=`echo "$APKFILE" | perl -p -e 's,-.*.apk,-signme.apk,g'`
-SIGNED=`echo "$APKFILE" | perl -p -e 's,-.*.apk,-signed.apk,g'`
+find platforms/android -name \*-debug.apk -o -name \*-release-unsigned.apk | while read APKFILE; do
+	SIGNME=`echo "$APKFILE" | perl -p -e 's,-[^\-]*.apk,-signme.apk,g'`
+	SIGNED=`echo "$APKFILE" | perl -p -e 's,-[^\-]*.apk,-signed.apk,g'`
 
-echo "source APK: $APKFILE"
-echo "signing: $SIGNME"
+	echo "source APK: $APKFILE"
+	echo "signing: $SIGNME"
 
-rsync -avr "$APKFILE" "$SIGNME"
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/share/android/android-release-key.keystore $EXTRA_ARGS "$SIGNME" ranger
-rm -f "$SIGNED"
-zipalign -v 4 "$SIGNME" "$SIGNED"
+	rsync -avr "$APKFILE" "$SIGNME"
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/share/android/android-release-key.keystore $EXTRA_ARGS "$SIGNME" ranger
+	rm -f "$SIGNED"
+	zipalign -v 4 "$SIGNME" "$SIGNED"
 
-echo "signed: $SIGNED"
+	echo "signed: $SIGNED"
+done
