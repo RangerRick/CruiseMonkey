635471bd947d58c77a2c8f539f0f66dcbb6fafc3
Benjamin Reed <ranger@opennms.org>
2016-01-22 14:38:08 -0500

    fix hook version updating

--- hooks/before_prepare/set_version_in_config.js
+++ hooks/before_prepare/set_version_in_config.js
@@ -22,7 +22,7 @@ if (rootdir) {
 	filestoreplace.forEach(function(val, index, array) {
 		var fullfilename = path.join(rootdir, val);
 		if (fs.existsSync(fullfilename)) {
-			replace_string_in_file(fullfilename, 'id="com.raccoonfink.cruisemonkey" version="[^"]*"', 'id="com.raccoonfink.cruisemonkey" version="' + configobj.version + '"');
+			replace_string_in_file(fullfilename, ' version="[^"]*"', ' version="' + configobj.version + '"');
 			replace_string_in_file(fullfilename, 'ios-CFBundleVersion="[^"]*"', 'ios-CFBundleVersion="' + configobj.build + '"');
 			replace_string_in_file(fullfilename, 'android-versionCode="[^"]*"', 'android-versionCode="' + configobj.build + '"');
 		} else {
